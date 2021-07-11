5b57a5d35bce0771da8af4f4353d1b604f91c3a0
Candymizer <86894274+Candymizer@users.noreply.github.com>
2021-07-11 16:33:50 -0700

    write movement script

--- Space Shoot'em Up/Assets/Scripts/PlayerMovement.cs
+++ Space Shoot'em Up/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // lots of code
-    // more code
+    public float moveSpeed = 8;
+
     
     // Start is called before the first frame update
     void Start()
